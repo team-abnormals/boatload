846ed34dd1c7b1437c60c38811e7fbb1523226bc
Markus1002 <51504784+Markus1002@users.noreply.github.com>
2021-01-15 19:26:15 +0200

    Fixed an error

--- src/main/java/com/minecraftabnormals/extraboats/core/other/ExtraBoatsRecipes.java
+++ src/main/java/com/minecraftabnormals/extraboats/core/other/ExtraBoatsRecipes.java
@@ -1,12 +1,13 @@
-package com.minecraftabnormals.extraboats.common.item.crafting;
+package com.minecraftabnormals.extraboats.core.other;
 
+import com.minecraftabnormals.extraboats.common.item.crafting.ChestBoatRecipe;
 import com.minecraftabnormals.extraboats.core.ExtraBoats;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class EBRecipes {
+public class ExtraBoatsRecipes {
 	public static final DeferredRegister<IRecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, ExtraBoats.MOD_ID);
 	public static final RegistryObject<IRecipeSerializer<?>> CHEST_BOAT = RECIPE_SERIALIZERS.register("crafting_special_chestboat", () -> ChestBoatRecipe.SERIALIZER);
 }

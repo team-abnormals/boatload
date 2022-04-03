a3cdc891f825d3bd61a3bff1cd953492a79c0c82
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-04-03 16:22:52 -0500

    Update gradle

--- src/main/java/com/teamabnormals/boatload/core/mixin/EntityMixin.java
+++ src/main/java/com/teamabnormals/boatload/core/mixin/EntityMixin.java
@@ -6,23 +6,19 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.GameRules;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-	@Shadow
-	public Level level;
 
 	@Inject(method = "causeFallDamage", at = @At(value = "HEAD"), cancellable = true)
 	private void dropBannerUponFalling(float distance, float damageMultiplier, DamageSource source, CallbackInfoReturnable<Boolean> info) {
-		if ((Object) this instanceof Boat) {
-			if (!this.level.isClientSide && !((Entity) (Object) this).isRemoved()) {
-				if (this.level.getGameRules().getBoolean(GameRules.RULE_DOENTITYDROPS)) {
+		if ((Object) this instanceof Boat boat) {
+			if (!boat.level.isClientSide && !((Entity) (Object) this).isRemoved()) {
+				if (boat.level.getGameRules().getBoolean(GameRules.RULE_DOENTITYDROPS)) {
 					((Entity) (Object) this).spawnAtLocation(((IDataManager) this).getValue(BoatloadTrackedData.BANNER));
 				}
 			}

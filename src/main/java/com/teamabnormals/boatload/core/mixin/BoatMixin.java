392b7b6fa2a9e7fdee770d7b96cd71d83f944392
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-03-06 13:23:15 -0600

    Tiny tweak

--- src/main/java/com/teamabnormals/boatload/core/mixin/BoatMixin.java
+++ src/main/java/com/teamabnormals/boatload/core/mixin/BoatMixin.java
@@ -14,16 +14,12 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Boat.class)
 public abstract class BoatMixin extends Entity {
-
-	@Shadow
-	protected float invFriction;
 
 	public BoatMixin(EntityType<?> entityTypeIn, Level worldIn) {
 		super(entityTypeIn, worldIn);

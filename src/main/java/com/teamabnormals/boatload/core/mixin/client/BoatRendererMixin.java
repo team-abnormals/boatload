a8ee3eb7f3b011c1ae7253a209efc4a6d960ab51
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2026-05-19 08:41:00 -0500

    Shearing Banners off

--- src/main/java/com/teamabnormals/boatload/core/mixin/client/BoatRendererMixin.java
+++ src/main/java/com/teamabnormals/boatload/core/mixin/client/BoatRendererMixin.java
@@ -5,6 +5,7 @@ import com.teamabnormals.boatload.core.other.BoatloadUtil;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.BoatRenderer;
 import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.vehicle.ChestBoat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
@@ -16,6 +17,8 @@ public abstract class BoatRendererMixin {
 
 	@Inject(method = "render(Lnet/minecraft/world/entity/vehicle/Boat;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", shift = Shift.AFTER, by = 2, target = "Lnet/minecraft/world/entity/vehicle/Boat;getBubbleAngle(F)F", ordinal = 0), cancellable = true)
 	private void render(Boat entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
-		BoatloadUtil.renderChest(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
+		if (entity instanceof ChestBoat) {
+			BoatloadUtil.renderChest(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
+		}
 	}
 }
--- src/main/java/com/teamabnormals/boatload/core/other/BoatloadEvents.java
+++ src/main/java/com/teamabnormals/boatload/core/other/BoatloadEvents.java
@@ -1,11 +1,22 @@
 package com.teamabnormals.boatload.core.other;
 
+import com.teamabnormals.blueprint.common.world.storage.tracking.IDataManager;
 import com.teamabnormals.boatload.core.Boatload;
 import com.teamabnormals.boatload.core.registry.BoatloadDataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent.EntityInteract;
 
 import java.util.function.Consumer;
 
@@ -13,9 +24,27 @@ import java.util.function.Consumer;
 public class BoatloadEvents {
 
 	@SubscribeEvent
+	public static void entityInteract(EntityInteract event) {
+		Level level = event.getLevel();
+		Player player = event.getEntity();
+		Entity target = event.getTarget();
+		ItemStack stack = event.getItemStack();
+
+		ItemStack banner = ((IDataManager) target).getValue(BoatloadTrackedData.BANNER);
+		if (stack.is(Tags.Items.TOOLS_SHEAR) && !banner.isEmpty()) {
+			level.playSound(null, target, SoundEvents.SHEEP_SHEAR, SoundSource.PLAYERS, 1.0F, 1.0F);
+			target.spawnAtLocation(banner, target.getBbHeight());
+			((IDataManager) target).setValue(BoatloadTrackedData.BANNER, ItemStack.EMPTY);
+			stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(event.getHand()));
+			event.setCancellationResult(InteractionResult.sidedSuccess(level.isClientSide()));
+			event.setCanceled(true);
+		}
+	}
+
+	@SubscribeEvent
 	public static void onItemTooltip(ItemTooltipEvent event) {
 		Consumer<Component> consumer = event.getToolTip()::add;
-		event.getItemStack().addToTooltip(BoatloadDataComponents.BANNER, event.getContext(), consumer, event.getFlags());
 		event.getItemStack().addToTooltip(BoatloadDataComponents.CHEST, event.getContext(), consumer, event.getFlags());
+		event.getItemStack().addToTooltip(BoatloadDataComponents.BANNER, event.getContext(), consumer, event.getFlags());
 	}
 }
